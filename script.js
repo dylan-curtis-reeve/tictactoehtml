9f27a024ea4802082145414f391de65cb49dbc80
dcr-lol <d.curtis.reeve@gmail.com>
2024-09-28 18:43:43 +1200

    Add end game state, add text centre styling for squares

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   let playerChoice = null;
   let currentPlayer = "X";
+  let gameOver = false;
   var playerSelectX = document.getElementById("x");
   var playerSelectY = document.getElementById("o");
   var playerSelect = document.getElementById("player-select");
@@ -27,11 +28,49 @@ document.addEventListener("DOMContentLoaded", function () {
   function squareEventListenerSetup(id) {
     var sq = document.getElementById(id);
     sq.addEventListener("click", function () {
-      if (playerChoice != null && sq.textContent === "") {
+      if (playerChoice != null && sq.textContent === "" && !gameOver) {
         sq.textContent = currentPlayer;
+        if (checkScore()) {
+          status.textContent = currentPlayer + " wins!";
+          gameOver = true;
+          return;
+        }
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         status.textContent = currentPlayer + "'s turn";
       }
     });
+  }
+  function checkScore() {
+    if (validateScore(squares[0], squares[1], squares[2])) {
+      return true;
+    } else if (validateScore(squares[3], squares[4], squares[5])) {
+      return true;
+    } else if (validateScore(squares[6], squares[7], squares[8])) {
+      return true;
+    } else if (validateScore(squares[0], squares[3], squares[6])) {
+      return true;
+    } else if (validateScore(squares[1], squares[4], squares[7])) {
+      return true;
+    } else if (validateScore(squares[2], squares[5], squares[8])) {
+      return true;
+    } else if (validateScore(squares[0], squares[4], squares[8])) {
+      return true;
+    } else if (validateScore(squares[2], squares[4], squares[6])) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function validateScore(box1, box2, box3) {
+    if (
+      box1.textContent === box2.textContent &&
+      box2.textContent === box3.textContent &&
+      box1.textContent !== ""
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 });
